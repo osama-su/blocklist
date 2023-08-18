cc87071fdf678ab2a8b850db36febeb521914c84
Osama Ahmed <osamaahmed.sus@gmail.com>
2023-08-18 17:27:51 +0300

    reset password

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -53,6 +54,88 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out',
+        ]);
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        // Validate the request...
+        $validated = $request->validate([
+            'phone_number' => 'required',
+        ]);
+
+        $user = User::where('phone_number', $validated['phone_number'])->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $token = rand(100000, 999999);
+
+        // save token in password_resets table
+        DB::table('password_reset_tokens')->insert([
+            'email' => $user->email,
+            'token' => $token,
+        ]);
+
+        // todo: Send SMS with $token to $user->phone_number
+
+        // Todo: remove token from response in production
+        return response()->json([
+            'message' => 'Token sent',
+            'token' => $token,
+        ]);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        // Validate the request...
+        $validated = $request->validate([
+            'phone_number' => 'required',
+            'token' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::where('phone_number', $validated['phone_number'])->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $passwordReset = DB::table('password_reset_tokens')->where('email', $user->email)->first();
+
+        if (!$passwordReset) {
+            return response()->json([
+                'message' => 'Invalid token',
+            ], 401);
+        }
+
+        if ($passwordReset->token != $validated['token']) {
+            return response()->json([
+                'message' => 'Invalid token',
+            ], 401);
+        }
+
+        $user->password = bcrypt($validated['password']);
+        $user->save();
+
+        $passwordReset->delete();
+
+        return response()->json([
+            'message' => 'Password reset successfully',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\BlacklistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middleware' => ['auth:sanctum'],
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
+
+Route::group([
+    'middleware' => ['auth:sanctum'],
+    'prefix' => 'blacklist'
+], function ($router) {
+    Route::get('/', [BlacklistController::class, 'index']);
+    Route::get('/all', [BlacklistController::class, 'all']);
+    Route::post('/', [BlacklistController::class, 'store']);
+    Route::get('/{id}', [BlacklistController::class, 'show']);
+    Route::put('/{id}', [BlacklistController::class, 'update']);
+    Route::delete('/{id}', [BlacklistController::class, 'destroy']);
+});
+
+
+
